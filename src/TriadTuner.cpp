bbcea8c3711e9f77f46e2ff82af6200cea7fe7f2
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2017-06-23 16:47:37 +0200

    Added a simple randomized tuning to the tuner of triad.

--- src/TriadTuner.cpp
+++ src/TriadTuner.cpp
@@ -18,6 +18,7 @@
 #include <algorithm>
 #include <iomanip>
 #include <ctime>
+#include <random>
 
 #include <configuration.hpp>
 
@@ -35,6 +36,7 @@ void initializeDeviceMemory(cl::Context & clContext, cl::CommandQueue * clQueue,
 int main(int argc, char * argv[]) {
   bool reInit = true;
   unsigned int nrIterations = 0;
+  unsigned int samplingFactor = 100;
   unsigned int clPlatformID = 0;
   unsigned int clDeviceID = 0;
   unsigned int vectorSize = 0;
@@ -42,13 +44,15 @@ int main(int argc, char * argv[]) {
   unsigned int maxItems = 0;
   unsigned int maxVector = 0;
   unsigned int inputSize = 0;
-  TuneBench::TriadConf conf;
 
   try {
     isa::utils::ArgumentList args(argc, argv);
 
     clPlatformID = args.getSwitchArgument< unsigned int >("-opencl_platform");
     clDeviceID = args.getSwitchArgument< unsigned int >("-opencl_device");
+    if ( args.getSwitch("-sampling") ) {
+      samplingFactor = args.getSwitchArgument< unsigned int >("-factor");
+    }
     nrIterations = args.getSwitchArgument< unsigned int >("-iterations");
     vectorSize = args.getSwitchArgument< unsigned int >("-vector");
     maxThreads = args.getSwitchArgument< unsigned int >("-max_threads");
@@ -56,7 +60,7 @@ int main(int argc, char * argv[]) {
     maxVector = args.getSwitchArgument< unsigned int >("-max_vector");
     inputSize = args.getSwitchArgument< unsigned int >("-input_size");
   } catch ( isa::utils::EmptyCommandLine & err ) {
-    std::cerr << argv[0] << " -opencl_platform ... -opencl_device ... -iterations ... -vector ... -max_threads ... -max_items ... -max_vector ... -input_size ... " << std::endl;
+    std::cerr << argv[0] << " -opencl_platform ... -opencl_device ... [-sampling -factor ...] -iterations ... -vector ... -max_threads ... -max_items ... -max_vector ... -input_size ... " << std::endl;
     return 1;
   } catch ( std::exception & err ) {
     std::cerr << err.what() << std::endl;
@@ -79,106 +83,124 @@ int main(int argc, char * argv[]) {
   }
   std::fill(C.begin(), C.end(), factor);
   std::fill(C_control.begin(), C_control.end(), factor);
+
+  // Run the control
   TuneBench::triad(A, B, C_control, static_cast< inputDataType >(factor));
 
-  std::cout << std::fixed << std::endl;
-  std::cout << "# inputSize vector nrThreadsD0 nrItemsD0 GB/s time stdDeviation COV" << std::endl << std::endl;
-
+  // Generate tuning configurations
+  std::vector<TuneBench::TriadConf> configurations;
   for ( unsigned int threads = vectorSize; threads <= maxThreads; threads += vectorSize ) {
-    conf.setNrThreadsD0(threads);
     for ( unsigned int items = 1; items <= maxItems; items++ ) {
-      conf.setNrItemsD0(items);
       for ( unsigned int vector = 1; vector <= maxVector; vector++ ) {
-        conf.setVector(vector);
-        if ( inputSize % (conf.getNrThreadsD0() * conf.getNrItemsD0() * conf.getVector()) != 0 ) {
+        if ( inputSize % (threads * items * vector) != 0 ) {
           continue;
-        } else if ( conf.getNrItemsD0() * conf.getVector() > maxItems ) {
+        } else if ( items * vector > maxItems ) {
           break;
         }
-
-        // Generate kernel
-        double gbytes = isa::utils::giga(static_cast< uint64_t >(inputSize) * sizeof(inputDataType) * 3.0);
-        cl::Event clEvent;
-        cl::Kernel * kernel;
-        isa::utils::Timer timer;
-        std::string * code = TuneBench::getTriadOpenCL(conf, inputDataName, factor);
-
-        if ( reInit ) {
-          delete clQueues;
-          clQueues = new std::vector< std::vector< cl::CommandQueue > >();
-          isa::OpenCL::initializeOpenCL(clPlatformID, 1, clPlatforms, &clContext, clDevices, clQueues);
-          try {
-            initializeDeviceMemory(clContext, &(clQueues->at(clDeviceID)[0]), &A, &A_d, &B, &B_d, &C_d);
-          } catch ( cl::Error & err ) {
-            return -1;
-          }
-          reInit = false;
-        }
-        try {
-          kernel = isa::OpenCL::compile("triad", *code, "-cl-mad-enable -Werror", clContext, clDevices->at(clDeviceID));
-        } catch ( isa::OpenCL::OpenCLError & err ) {
-          std::cerr << err.what() << std::endl;
-          delete code;
-          break;
-        }
-        delete code;
-
-        cl::NDRange global(inputSize / (conf.getNrItemsD0() * conf.getVector()));
-        cl::NDRange local(conf.getNrThreadsD0());
-
-        kernel->setArg(0, A_d);
-        kernel->setArg(1, B_d);
-        kernel->setArg(2, C_d);
-
-        try {
-          // Warm-up run
-          clQueues->at(clDeviceID)[0].finish();
-          clQueues->at(clDeviceID)[0].enqueueNDRangeKernel(*kernel, cl::NullRange, global, local, 0, &clEvent);
-          clEvent.wait();
-          // Tuning runs
-          for ( unsigned int iteration = 0; iteration < nrIterations; iteration++ ) {
-            timer.start();
-            clQueues->at(clDeviceID)[0].enqueueNDRangeKernel(*kernel, cl::NullRange, global, local, 0, &clEvent);
-            clEvent.wait();
-            timer.stop();
-          }
-          clQueues->at(clDeviceID)[0].enqueueReadBuffer(C_d, CL_TRUE, 0, C.size() * sizeof(inputDataType), reinterpret_cast< void * >(C.data()), 0, &clEvent);
-          clEvent.wait();
-        } catch ( cl::Error & err ) {
-          std::cerr << "OpenCL kernel execution error (";
-          std::cerr << conf.print();
-          std::cerr << "), (";
-          std::cerr << isa::utils::toString(inputSize / conf.getNrItemsD0()) << "): ";
-          std::cerr << isa::utils::toString(err.err()) << std::endl;
-          delete kernel;
-          if ( err.err() == -4 || err.err() == -61 ) {
-            return -1;
-          }
-          reInit = true;
-          break;
-        }
-        delete kernel;
-
-        bool error = false;
-        for ( unsigned int item = 0; item < C.size(); item++ ) {
-          if ( !isa::utils::same(C[item], C_control[item]) ) {
-            std::cerr << "Output error (" << conf.print() << ")." << std::endl;
-            error = true;
-            break;
-          }
-        }
-        if ( error ) {
-          continue;
-        }
-
-        std::cout << inputSize << " ";
-        std::cout << conf.print() << " ";
-        std::cout << std::setprecision(3);
-        std::cout << gbytes / timer.getAverageTime() << " ";
-        std::cout << std::setprecision(6);
-        std::cout << timer.getAverageTime() << " " << timer.getStandardDeviation() << " " << timer.getCoefficientOfVariation() << std::endl;
+        TuneBench::TriadConf configuration;
+        configuration.setNrThreadsD0(threads);
+        configuration.setNrItemsD0(items);
+        configuration.setVector(vector);
+        configurations.push_back(configuration);
       }
     }
+  }
+
+  std::cout << std::fixed << std::endl;
+  std::cout << "# inputSize *configuration* GB/s time stdDeviation COV" << std::endl << std::endl;
+
+  for ( auto configuration = configurations.begin(); configuration != configurations.end(); ++configuration ) {
+    if ( samplingFactor < 100 ) {
+      // Sample the configurations
+      std::default_random_engine randomEngine;
+      std::uniform_int_distribution<unsigned int> uniformDistribution(0, 99);
+      unsigned int randomValue = uniformDistribution(randomEngine);
+
+      if ( randomValue >= samplingFactor ) {
+        break;
+      }
+    }
+    // Generate kernel
+    double gbytes = isa::utils::giga(static_cast< uint64_t >(inputSize) * sizeof(inputDataType) * 3.0);
+    cl::Event clEvent;
+    cl::Kernel * kernel;
+    isa::utils::Timer timer;
+    std::string * code = TuneBench::getTriadOpenCL(*configuration, inputDataName, factor);
+
+    if ( reInit ) {
+      delete clQueues;
+      clQueues = new std::vector< std::vector< cl::CommandQueue > >();
+      isa::OpenCL::initializeOpenCL(clPlatformID, 1, clPlatforms, &clContext, clDevices, clQueues);
+      try {
+        initializeDeviceMemory(clContext, &(clQueues->at(clDeviceID)[0]), &A, &A_d, &B, &B_d, &C_d);
+      } catch ( cl::Error & err ) {
+        return -1;
+      }
+      reInit = false;
+    }
+    try {
+      kernel = isa::OpenCL::compile("triad", *code, "-cl-mad-enable -Werror", clContext, clDevices->at(clDeviceID));
+    } catch ( isa::OpenCL::OpenCLError & err ) {
+      std::cerr << err.what() << std::endl;
+      delete code;
+      break;
+    }
+    delete code;
+
+    cl::NDRange global(inputSize / ((*configuration).getNrItemsD0() * (*configuration).getVector()));
+    cl::NDRange local((*configuration).getNrThreadsD0());
+
+    kernel->setArg(0, A_d);
+    kernel->setArg(1, B_d);
+    kernel->setArg(2, C_d);
+
+    try {
+      // Warm-up run
+      clQueues->at(clDeviceID)[0].finish();
+      clQueues->at(clDeviceID)[0].enqueueNDRangeKernel(*kernel, cl::NullRange, global, local, 0, &clEvent);
+      clEvent.wait();
+      // Tuning runs
+      for ( unsigned int iteration = 0; iteration < nrIterations; iteration++ ) {
+        timer.start();
+        clQueues->at(clDeviceID)[0].enqueueNDRangeKernel(*kernel, cl::NullRange, global, local, 0, &clEvent);
+        clEvent.wait();
+        timer.stop();
+      }
+      clQueues->at(clDeviceID)[0].enqueueReadBuffer(C_d, CL_TRUE, 0, C.size() * sizeof(inputDataType), reinterpret_cast< void * >(C.data()), 0, &clEvent);
+      clEvent.wait();
+    } catch ( cl::Error & err ) {
+      std::cerr << "OpenCL kernel execution error (";
+      std::cerr << (*configuration).print();
+      std::cerr << "), (";
+      std::cerr << isa::utils::toString(inputSize / (*configuration).getNrItemsD0()) << "): ";
+      std::cerr << isa::utils::toString(err.err()) << std::endl;
+      delete kernel;
+      if ( err.err() == -4 || err.err() == -61 ) {
+        return -1;
+      }
+      reInit = true;
+      break;
+    }
+    delete kernel;
+
+    bool error = false;
+    for ( unsigned int item = 0; item < C.size(); item++ ) {
+      if ( !isa::utils::same(C[item], C_control[item]) ) {
+        std::cerr << "Output error (" << (*configuration).print() << ")." << std::endl;
+        error = true;
+        break;
+      }
+    }
+    if ( error ) {
+      continue;
+    }
+
+    std::cout << inputSize << " ";
+    std::cout << (*configuration).print() << " ";
+    std::cout << std::setprecision(3);
+    std::cout << gbytes / timer.getAverageTime() << " ";
+    std::cout << std::setprecision(6);
+    std::cout << timer.getAverageTime() << " " << timer.getStandardDeviation() << " " << timer.getCoefficientOfVariation() << std::endl;
   }
   std::cout << std::endl;
 
