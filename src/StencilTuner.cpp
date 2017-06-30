f623d2863315141faecb766d5bc1b509fd8ab72d
Alessio Sclocco <alessio@sclocco.eu>
2017-06-30 15:28:26 +0200

    Casting the error to right data type.

--- src/StencilTuner.cpp
+++ src/StencilTuner.cpp
@@ -194,7 +194,7 @@ int main(int argc, char * argv[]) {
     bool error = false;
     for ( unsigned int y = 0; y < matrixWidth; y++ ) {
       for ( unsigned int x = 0; x < matrixWidth; x++ ) {
-        if ( !isa::utils::same(output[(y * isa::utils::pad(matrixWidth, padding)) + x], output_c[(y * isa::utils::pad(matrixWidth, padding)) + x], 0.001f) ) {
+        if ( !isa::utils::same(output[(y * isa::utils::pad(matrixWidth, padding)) + x], output_c[(y * isa::utils::pad(matrixWidth, padding)) + x], static_cast<inputDataType>(0.001)) ) {
           std::cerr << "Output error (" << (*configuration).print() << ")." << std::endl;
           error = true;
           break;
