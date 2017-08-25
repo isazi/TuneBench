bf7bed5262fcb99a4c82e3e095339addcb352afb
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2017-08-25 14:17:58 +0200

    First implementation of a benchmark using Ben's kernel_tuner.

--- TuneBench/triad.py
+++ TuneBench/triad.py
@@ -2,29 +2,24 @@
 Triad benchmark.
 """
 
-# Templates
-TEMPLATE_OPENCL = """__kernel void(__global const <%VECTOR_DATA%> * const restrict A, __global const <%VECTOR_DATA%> * const restrict B, __global <%VECTOR_DATA%> * const restrict C) {
+import numpy
+from kernel_tuner import tune_kernel
+
+# Global variables
+FACTOR = 42.0
+
+# Code templates
+TEMPLATE_OPENCL = """__kernel void triad(__global const <%VECTOR_DATA%> * const restrict A, __global const <%VECTOR_DATA%> * const restrict B, __global <%VECTOR_DATA%> * const restrict C) {
 unsigned int item = (get_group_id(0) * <%ITEMS_PER_WORKGROUP%>) + get_local_id(0);
 <%COMPUTE%>
 }\n"""
-TEMPLATE_CUDA = """__global__ void(const <%VECTOR_DATA%> * const restrict A, const <%VECTOR_DATA%> * const restrict B, <%VECTOR_DATA%> * const restrict C) {
+TEMPLATE_CUDA = """__global__ void triad(const <%VECTOR_DATA%> * const restrict A, const <%VECTOR_DATA%> * const restrict B, <%VECTOR_DATA%> * const restrict C) {
 unsigned int item = (blockIdx.x * <%THREADS_PER_BLOCK%>) + threadIdx.x;
 <%COMPUTE%>
 }\n"""
 TEMPLATE_COMPUTE = """C[item + <%OFFSET%>] = A[item + <%OFFSET%>] + (<%FACTOR%> * B[item + <%OFFSET%>]);"""
 
-# Code generator
-def generate_triad_code(language, configuration, factor):
-    """
-    Generate the source code for triad.
-    """
-    code = str()
-    if language == "opencl":
-        code = TEMPLATE_OPENCL.replace("<%VECTOR_DATA%>", configuration["vector_data"])
-        code = code.replace("<%ITEMS_PER_WORKGROUP%>", str(int(configuration["threads_dim0"]) * int(configuration["items_dim0"])))
-    elif language == "cuda":
-        code = TEMPLATE_CUDA.replace("<%VECTOR_DATA%>", configuration["vector_data"])
-        code = code.replace("<%THREADS_PER_BLOCK%>", str(int(configuration["threads_dim0"]) * int(configuration["items_dim0"])))
+def generate_compute_code(configuration):
     compute_code = str()
     for item in range(int(configuration["items_dim0"])):
         temp = TEMPLATE_COMPUTE
@@ -32,37 +27,56 @@ def generate_triad_code(language, configuration, factor):
             temp = temp.replace(" + <%OFFSET%>", "")
         else:
             temp = temp.replace("<%OFFSET%>", str(item * int(configuration["threads_dim0"])))
-        temp = temp.replace("<%FACTOR%>", str(factor))
+        temp = temp.replace("<%FACTOR%>", str(FACTOR))
         # Format the code
         if item != (configuration["items_dim0"] - 1):
             temp = temp + "\n"
         compute_code = compute_code + temp
-    code = code.replace("<%COMPUTE%>", compute_code)
+
+    return compute_code
+
+# OpenCL code generator
+def generate_code_OpenCL(configuration):
+    """
+    OpenCL code generator for the Triad benchmark.
+    """
+    code = str()
+    code = TEMPLATE_OPENCL.replace("<%VECTOR_DATA%>", configuration["vector_data"])
+    code = code.replace("<%ITEMS_PER_WORKGROUP%>", str(int(configuration["threads_dim0"]) * int(configuration["items_dim0"])))
+    code = code.replace("<%COMPUTE%>", generate_compute_code(configuration))
 
     return code
 
-# Configurations generator
-def generate_triad_configurations(parameters):
+# CUDA code generator
+def generate_code_CUDA(configuration):
     """
-    Generate all configurations for triad.
+    CUDA code generator for the Triad benchmark.
     """
-    configurations = list()
-    for vector in range(parameters["vector_data"]["start"], parameters["vector_data"]["stop"], parameters["vector_data"]["increment"]):
-        if vector == 1:
-            data_type = parameters["vector_data"]["type"]
-        else:
-            data_type = parameters["vector_data"]["type"] + str(vector)
-        for threads in range(parameters["threads_dim0"]["start"], parameters["threads_dim0"]["stop"], parameters["threads_dim0"]["increment"]):
-            for items in range(parameters["items_dim0"]["start"], parameters["items_dim0"]["stop"], parameters["items_dim0"]["increment"]):
-                configurations.append(dict(vector_data=data_type, threads_dim0=threads, items_dim0=items))
-    return configurations
+    code = str()
+    code = TEMPLATE_CUDA.replace("<%VECTOR_DATA%>", configuration["vector_data"])
+    code = code.replace("<%THREADS_PER_BLOCK%>", str(int(configuration["threads_dim0"]) * int(configuration["items_dim0"])))
+    code = code.replace("<%COMPUTE%>", generate_compute_code(configuration))
 
-# Control implementation
-def triad_control(A, B, factor):
+    return code
+
+def tune(input_size, language, constraints):
     """
-    Control CPU implementation.
+    Triad benchmark tuner.
     """
-    control = list()
-    for item in range(0, len(A)):
-        control.append(A[item] + (B[item] * factor))
-    return control
+
+    A = numpy.random.randn(input_size).astype(numpy.float32)
+    B = numpy.random.randn(input_size).astype(numpy.float32)
+    C = numpy.random.randn(input_size).astype(numpy.float32)
+    kernel_arguments = [A, B, C]
+
+    tuning_parameters = dict()
+    tuning_parameters["vector_data"] = [2**i for i in range(5)]
+    tuning_parameters["threads_dim0"] = [threads for threads in range(constraints["threads_dim0_min"], constraints["threads_dim0_max"], constraints["threads_dim0_step"])]
+    tuning_parameters["items_dim0"] = [items for items in range(constraints["items_dim0_min"], constraints["items_dim0_max"], constraints["items_dim0_step"])]
+    
+    if language == "OpenCL":
+        results = tune_kernel("triad", generate_code_OpenCL, input_size, kernel_arguments, tuning_parameters, lang=language)
+    else:
+        results = tune_kernel("triad", generate_code_CUDA, input_size, kernel_arguments, tuning_parameters, lang=language)
+
+    print(results)
