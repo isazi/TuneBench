b21b05dc5826f26195f801ad0771f2f052b7d569
Alessio Sclocco <alessio@sclocco.eu>
2017-10-20 15:46:55 +0200

    The difficulty is now represented by the distribution of configurations over performance.

--- analysis/difficulty.py
+++ analysis/difficulty.py
@@ -16,6 +16,7 @@
 
 def difficulty(db_queue, table, benchmark, scenario):
     """Computes the tuning difficulty for the given scenario."""
+    distribution = list()
     metrics = ""
     if benchmark.lower() == "triad":
         metrics = "GBs,"
@@ -27,12 +28,17 @@ def difficulty(db_queue, table, benchmark, scenario):
         metrics = "GFLOPs,"
     elif benchmark.lower() == "correlator":
         metrics = "GFLOPs,"
-    db_queue.execute("SELECT COUNT(id),MAX(" + metrics.rstrip(",") + ") FROM " + table + " WHERE " + scenario)
+    db_queue.execute("SELECT COUNT(id),MIN(" + metrics.rstrip(",") + "),MAX(" + metrics.rstrip(",") + ") FROM " + table + " WHERE " + scenario)
     results = db_queue.fetchall()
-    total_confs = results[0][0] - 1
-    threshold = results[0][1] - ((10 * results[0][1]) / 100)
-    db_queue.execute("SELECT COUNT(id) FROM " + table + " WHERE (" + scenario + " AND " + metrics.rstrip(",") + " >= " + str(threshold) + ")")
-    results = db_queue.fetchall()
-    good_confs = results[0][0] - 1
-    score = 100 - ((good_confs * 100) / total_confs)
-    return [score]
+    total_confs = results[0][0]
+    minimum = results[0][1]
+    maximum = results[0][2]
+    percentile_range = (maximum - minimum) / 10
+    for percentile in range(1, 11):
+        db_queue.execute("SELECT COUNT(id) FROM " + table + " WHERE (" + scenario + " AND " + metrics.rstrip(",") + " >= " + str(minimum + ((percentile - 1) * percentile_range)) + " AND " + metrics.rstrip(",") + " < " + str(minimum + (percentile * percentile_range)) + ")")
+        results = db_queue.fetchall()
+        distribution.append(results[0][0])
+    distribution.append(distribution.pop() + 1)
+    for percentile in range(0, 10):
+        distribution[percentile] = (distribution[percentile] * 100) / total_confs
+    return distribution
