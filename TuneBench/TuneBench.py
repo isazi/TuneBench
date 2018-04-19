f2f4248d1e1e85e3e907ddcc3151e657ded1cb3d
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-04-19 16:25:45 +0200

    Fixed an error with command line parser.

--- TuneBench/TuneBench.py
+++ TuneBench/TuneBench.py
@@ -8,7 +8,7 @@ def parseCommandLine():
     parser = argparse.ArgumentParser(description="TuneBench: a fully tunable benchmark for many-core accelerators")
     parser.add_argument("--language", help="Language: CUDA or OpenCL", choices=["CUDA", "OpenCL"], required=True)
     parser_benchmarks = parser.add_subparsers(dest="benchmark")
-    parser_triad = parser_benchmarks.add_subparsers("triad")
+    parser_triad = parser_benchmarks.add_parser("triad")
     parser_triad.add_argument("--type", help="Data type on the device.", type=str, required=True)
     parser_triad.add_argument("--numpy_type", help="Data type on the host.", type=str, required=True)
     parser_triad.add_argument("--input_size", help="Input size", type=int, required=True)
